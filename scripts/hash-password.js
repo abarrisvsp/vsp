7a24e907cadbb4e7f4f639cfb9d779418d55968f
Neil Barris <nbarris11@gmail.com>
2026-05-27 16:04:03 -0400

    feat: add bcrypt password hash script

---
+++ scripts/hash-password.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs');
+
+const password = process.argv[2];
+if (!password) {
+  console.error('Usage: node scripts/hash-password.js "yourpassword"');
+  process.exit(1);
+}
+
+bcrypt.hash(password, 12).then((hash) => {
+  console.log('\nADMIN_PASSWORD_HASH=' + hash + '\n');
+});
